f9e4b54b207cdad9a27ea7c0be740846f6cece84
Matt Gibson <git@mgibson.dev>
2025-11-13 13:46:33 -0800

    add openssh for dev machines

---
+++ roles/development/openssh.nix
@@ -0,0 +1,17 @@
+{lib, ...}: {
+  services.openssh = {
+    enable = true;
+    settings = {
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = lib.mkForce false;
+      PubkeyAuthentication = lib.mkForce true;
+      PermitRootLogin = lib.mkForce "no";
+    };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+  };
+}
