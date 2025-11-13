7affcce8417d9ef6b3fff6c72acca7038b66cc9e
Matt Gibson <git@mgibson.dev>
2025-11-13 13:44:44 -0800

    powershell 10k config

--- home-manager/mgibson/default.nix
+++ home-manager/mgibson/default.nix
@@ -18,10 +18,11 @@
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
-      #      {
-      #        name = "powerlevel10k-config";
-      #        src = ./p10k-config;
-      #      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = "p10k.zsh";
+      }
     ];
   };
 
