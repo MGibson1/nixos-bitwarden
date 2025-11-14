f3ba0d527c3173bc253734fdc7d9dc8da72286ca
Matt Gibson <mgibson@bitwarden.com>
2025-11-14 09:02:42 -0800

    enable tpm

--- hosts/steeltoes/default.nix
+++ hosts/steeltoes/default.nix
@@ -37,5 +37,11 @@
     EDITOR = "micro";
   };
 
+  # Enable tpm
+  security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+  users.users.${vars.user}.extraGroups = ["tss"]; # tss group has access to TPM devices
+
   system.stateVersion = "25.05";
 }
