af5a6a7b8608b113f91c188aac16af1198a64b5b
Matt Gibson <mgibson@bitwarden.com>
2025-11-14 21:50:47 -0800

    sign commits

--- home-manager/mgibson/default.nix
+++ home-manager/mgibson/default.nix
@@ -27,5 +27,20 @@
     ];
   };
 
+  programs.git = {
+    enable = true;
+    userName = "Matt Gibson";
+    userEmail = "mgibson@bitwarden.com";
+    extraConfig = {
+      # Sign all commits using ssh key
+      commit.gpgsign = true;
+      tag.gpgsign = true;
+      gpg.format = "ssh";
+      # Generate this key outside of home-manager with
+      # ssh-keygen -t ed25519
+      user.signingkey = "/home/mgibson/.ssh/bw-signing.pub";
+    };
+  };
+
   home.stateVersion = "25.05";
 }
