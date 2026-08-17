9ad3fce4eb37b8babc163d322a10c3cfc557cd66
Matt Gibson <mgibson@bitwarden.com>
2026-08-17 15:09:50 -0700

    Use delta for better lazygit diffs

--- home-manager/mgibson/default.nix
+++ home-manager/mgibson/default.nix
@@ -1,6 +1,4 @@
 {
-  config,
-  pkgs,
   vars,
   ...
 }:
@@ -9,6 +7,7 @@
     ./gnome.nix
     ../roles/developer
     ../roles/creative
+    ./git.nix
     ./nushell
     ./helix.nix
     ./lazygit.nix
@@ -16,50 +15,6 @@
 
     ../common.nix
   ];
-
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "Matt Gibson";
-        email = "mgibson@bitwarden.com";
-        # IMPERATIVE: Generate this key outside of home-manager with
-        # ssh-keygen -t ed25519
-        signingkey = "${vars.home-dir}/${vars.user}/.ssh/bw-signing.pub";
-      };
-      # Sign all commits using ssh key
-      commit.gpgsign = true;
-      tag.gpgsign = true;
-      gpg.format = "ssh";
-      credential = {
-        "https://github.com".helper = "${pkgs.gh}/bin/gh auth git-credential";
-        "https://gist.github.com".helper = "${pkgs.gh}/bin/gh auth git-credential";
-      };
-      core.excludesFile = "${vars.home-dir}/${vars.user}/.gitignore_global";
-      init.defaultBranch = "main";
-    };
-  };
-
-  home.file.".gitignore_global".text = ''
-    # direnv files and associated flakes
-    .direnv
-    .envrc
-    flake.lock
-    flake.nix
-
-    # rust targets
-    **/target
-
-    # structurizr internals
-    **/.structurizr
-
-    # server dev/helpers should be ignored
-    dev/helpers
-
-    # claude local files
-    **/.claude/settings.local.json
-    CLAUDE.local.md
-  '';
 
   # need to configure any sessions to source this file on startup
   # it'd be nice if home-manager could do this, but it seems to only support
--- home-manager/mgibson/lazygit.nix
+++ home-manager/mgibson/lazygit.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    lazygit
-  ];
+{ ... }: {
+  programs.lazygit = {
+    enable = true;
+    enableBashIntegration = true;
+    enableNushellIntegration = true;
+    settings = {
+      git.pagers = [
+        {
+          pager = "delta --dark --paging=never --features=split-diff";
+        }
+        {
+          pager = "delta --dark --paging=never --features=stack-diff";
+        }
+      ];
+    };
+  };
 }
