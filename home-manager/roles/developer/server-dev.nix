484d9bac790832186e0aa809e997481d642f6e8f
Matt Gibson <mgibson@bitwarden.com>
2026-08-17 17:12:10 -0700

    Enable c-sharp lsp for helix

--- home-manager/roles/developer/server-dev.nix
+++ home-manager/roles/developer/server-dev.nix
@@ -1,5 +1,35 @@
-{...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   home.sessionPath = [
     "$HOME/.dotnet/tools"
   ];
+
+  home.packages = [
+    pkgs.roslyn-ls
+  ];
+
+  programs.helix.languages = lib.mkIf (config.programs.helix.enable) {
+    language-server.roslyn-language-server = {
+      command = "Microsoft.CodeAnalysis.LanguageServer";
+      args = [
+        "--stdio"
+        "--autoLoadProjects"
+      ];
+    };
+
+    language = [
+      {
+        name = "c-sharp";
+        language-servers = [
+          "roslyn-language-server"
+          "helix-assist"
+        ];
+      }
+    ];
+  };
 }
